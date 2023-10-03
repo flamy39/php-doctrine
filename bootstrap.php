2d7c627e0a809fe5f56c27251eb1fd7665d802f3
Franck LAMY <franck.lamy39@free.fr>
2023-10-03 09:51:38 +0200

    configuration de doctrine

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,28 @@
 <?php
 
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
 require_once "vendor/autoload.php";
+
+// Création de la configuration de Doctrine
+// Utilisation des attributs
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    paths: array(__DIR__."/src"),
+    isDevMode: true,
+);
+// Configuration de la connexion à la base de données
+$connection = DriverManager::getConnection([
+    'driver' => 'pdo_mysql',
+    'host' => 'localhost',
+    'dbname' => 'test',
+    'user' => 'root',
+    'password' => ''
+], $config);
+
+// Création de l'entity manager
+$entityManager = EntityManager::create($connection, $config);
+
+
 
